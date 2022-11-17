3ccaafbdeebfc7005bdefd383014f7800bd61e87
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2022-11-17 11:09:06 -0800

    Established User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,24 @@
 // backend/routes/api/users.js
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+// sign up endpoint
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user
+      });
+    }
+  );
 
 
-module.exports = router;
+  module.exports = router;

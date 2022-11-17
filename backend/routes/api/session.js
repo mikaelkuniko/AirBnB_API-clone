36b7c867b73ca9780f4fe92ebcd4b6936a513c64
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2022-11-17 14:35:39 -0800

    Established User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,10 +4,26 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
-router.post(
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+   // log in
+   router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
 
@@ -50,6 +66,8 @@ router.post(
       } else return res.json({});
     }
   );
+
+
 
 
 

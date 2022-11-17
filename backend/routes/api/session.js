37a9a2bc9e089320c2b0e02c43a8d43be193b456
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2022-11-17 10:58:25 -0800

    Established User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -29,6 +29,16 @@ router.post(
     }
   );
 
-  
+  //log out
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
+
+
 
 module.exports = router;

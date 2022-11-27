c3c4f1000ca6875eb1da032321152bd9ee10ec64
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2022-11-26 20:39:44 -0800

    Created review images model, migration, and seeds

---
+++ backend/db/seeders/20221127043747-demo-review-images.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'ReviewImages';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(options, [
+      {
+        reviewId: 1,
+        url: 'star.png'
+      },
+      {
+        reviewId: 2,
+        url: 'silverstar.png'
+      },
+      {
+        reviewId: 3,
+        url: 'goldstar.png'
+      },
+    ], {});
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      reviewId: { [Op.in]: [1,2,3] }
+    }, {});
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

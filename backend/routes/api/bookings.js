aa0fc2800879bb1ca3a9425b13f5f9e10c5b90ed
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2022-11-28 14:44:15 -0800

    Created endpoint for get current bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,5 +9,37 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { application } = require('express');
 const review = require('../../db/models/review');
 
+// get users current bookings
+router.get('/current', requireAuth, async(req,res,next)=> {
+    const {user} = req;
+    let myBookings = await Booking.findAll({
+        where: {
+            userId: user.id
+        },
+        include: [{
+            model: Spot,
+            attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+            include: [{
+                model: SpotImage,
+                attributes: ['url']
+            }]
+        }]
+    })
+
+    let currBookingsArr = [];
+    myBookings.forEach(booking => {
+        currBookingsArr.push(booking.toJSON())
+    })
+    currBookingsArr.forEach(booking => {
+        let url = booking.Spot.SpotImages[0].url
+        booking.Spot.previewImage = url;
+        delete booking.Spot.SpotImages
+    })
+    res.status(200)
+    return res.json({
+        Bookings: currBookingsArr
+    });
+})
+
 
 module.exports = router

8ac246420d62cdc2652345f7c9bfcaeee423bc93
mikaelkuniko@gmail.com <mikaelkuniko@Mikaels-Macbook.local>
2022-12-18 13:26:52 -0800

    Home page css done

--- frontend/src/components/SpotsCards/index.js
+++ frontend/src/components/SpotsCards/index.js
@@ -9,9 +9,13 @@ const SpotCard = ({ id ,name, address, city, state, country, lat, lng, descripti
             <img src={previewImage} alt='spot image' id="spot-image"/>
           </div>
           <div className="spot-info-container">
+            <div className="top-info-div">
             <h3>{city}, {state} </h3>
             <p><i class="fa-sharp fa-solid fa-star"/> {avgRating ? avgRating : ' New Location'}</p>
-            <p>${price} per night</p>
+            </div>
+            <span className='bottom-info-span'>
+            <p id='price-text'>${price}</p><p id='night-text'>night</p>
+            </span>
           </div>
         {/* <h2>{name}</h2> */}
         </NavLink>
